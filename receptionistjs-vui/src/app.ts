7ab26ed0b20c1693dadc274da15944c5e938a1a8
Frederik Hoffmann <frederik.hoffmann@sparqs.io>
2019-03-23 16:50:48 +0100

    add mockData and work on EventIntent

--- receptionistjs-vui/src/app.ts
+++ receptionistjs-vui/src/app.ts
@@ -7,7 +7,6 @@ import {Alexa} from 'jovo-platform-alexa';
 import {GoogleAssistant} from 'jovo-platform-googleassistant';
 import {JovoDebugger} from 'jovo-plugin-debugger';
 import {FileDb} from 'jovo-db-filedb';
-import mock = jest.mock;
 
 const app = new App();
 
@@ -33,25 +32,46 @@ app.setHandler({
     },
 
     WelcomeIntent() {
-        this.ask('Welcome to DOHACKJS, What\'s your name?', 'Please tell me your name?')
+        let speech = 'Welcome, I am your receptionist for today. What\'s your name?';
+        let reprompt = 'Please tell me your name?';
+        this.ask(speech, reprompt)
     },
 
     EventState: {
         YesIntent() {
-            this.tell('Yes')
+            const events = getEvents();
+            const eventNames = [];
+            for (const event of events) {
+                eventNames.push(event.name);
+            }
+            // @ts-ignore
+            let speech = this.speechBuilder();
+            for (const eventName of eventNames) {
+                // @ts-ignore
+                speech.addText(eventName + ', ');
+            }
+
+            // @ts-ignore
+            speech.addBreak('1000ms').addText('To which of these events do you want to go?');
+            // @ts-ignore
+            this.ask(speech, 'Please tell me to which you want to go');
         },
 
         NoIntent() {
-            this.tell('No')
+            this.tell('Okay, I am sorry, that i could\'nt help you');
         }
     },
     EventIntent() {
         const events = getEvents();
-        let found = events.filter(value => value.name.includes(this.$inputs.eventName.value)).length > 0;
-        if (found) {
+        let foundEvent = events.filter(value => value.name.includes(this.$inputs.eventName.value))[0];
+        if (foundEvent) {
+            if (validateAttendee(foundEvent.id)) {
+                this.tell('Great, I was able to find you in my notes. ');
+            }
             this.tell('Okay, ' + this.$inputs.eventName.value + '. Give me a second, while I look for you in my files.');
         } else {
-            this.followUpState('EventState').ask('Sorry, I could not find the your Event. Do you want me to tell you, which Events are availiable?', '')
+            let speech = 'Sorry, I could not find the your Event. Do you want me to tell you, which Events are available?';
+            this.followUpState('EventState').ask(speech, 'Please answer with yes or no.')
         }
     }
 });
@@ -63,12 +83,19 @@ function getEvents() {
             name: 'DOHACKJS',
             type: 'conference',
             description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et'
+        },
+        {
+            id: '5',
+            name: 'Wrestlecon',
+            type: 'conference',
+            description: 'A Super Convention for Wrestling'
+
         }
     ];
     return mockEvents;
 }
 
-function validateAttendee(eventID: number) {
+function validateAttendee(eventID: string) {
     const mockAttendees = [
         {
             id: "3242394235",
