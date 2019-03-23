777bfe6a04a3129df5eac09ea55c018b39587549
Nils F <nils.fenzl@sparqs.io>
2019-03-23 18:21:08 +0100

    added registration

--- receptionist-ui/src/app/registration/registration.page.ts
+++ receptionist-ui/src/app/registration/registration.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
@@ -12,16 +13,38 @@ export class RegistrationPage implements OnInit {
   eventName: string;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private http: HttpClient) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.eventId = params['id'];
       this.eventName = params['name'];
-
-      console.log(this.eventId);
-  });
+    });
   }
 
+  register(form) {
 
+    const data = {
+      'id': this.eventId,
+      'firstName': form.form.controls.firstName.value,
+      'lastName': form.form.controls.lastName.value,
+      'eMail': form.form.controls.eMail.value,
+      'street': form.form.controls.street.value,
+      'postCode': form.form.controls.postCode.value,
+      'city': form.form.controls.city.value,
+      'birthDate': form.form.controls.birthDate.value,
+      'phoneNumber': form.form.controls.phoneNumber.value,
+    };
+
+    this.http.post('http://localhost:3000/events', JSON.stringify(data))
+    .toPromise()
+    .then((value) => {
+      console.log(value);
+    })
+    .catch((err) => {
+      console.log(err);
+    }
+    );
+  }
 }
