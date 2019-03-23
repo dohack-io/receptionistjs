6d27d36d0e7d1aab21e04929f7dfc995d53fad5d
Leon Jäkel <info@leon-jaekel.com>
2019-03-23 13:26:52 +0100

    Setups HttpClientModule for angular

---
+++ receptionist-ui/src/app/events/events.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.page.html',
+  styleUrls: ['./events.page.scss'],
+})
+export class EventsPage implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+}
