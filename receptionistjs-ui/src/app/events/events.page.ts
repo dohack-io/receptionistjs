753ee85327162ce184cffb91975995b43a94964f
Leon Jäkel <info@leon-jaekel.com>
2019-03-24 06:54:28 +0100

    Adds loading spinner

--- receptionistjs-ui/src/app/events/events.page.ts
+++ receptionistjs-ui/src/app/events/events.page.ts
@@ -1,36 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.page.html',
-  styleUrls: ['./events.page.scss'],
+  styleUrls: ['./events.page.scss']
 })
 export class EventsPage implements OnInit {
-
   private events: any = [];
 
-  constructor(private http: HttpClient,
-              private router: Router) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private loadingController: LoadingController,
+  ) {}
+
+  async ngOnInit() {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+    await loading.present();
+    await this.http
+      .get('http://localhost:5000/events')
+      .toPromise()
+      .then(value => {
+        console.log(value);
+        this.events = value;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
   }
 
-  ngOnInit() {
-    this.http.get('http://localhost:5000/events').toPromise().then((value) => {
-      console.log(value);
-      this.events = value;
-    }).catch((err) => {
-      console.log(err);
+  onOpenEvent(eventId: number, eventName: string) {
+    this.router.navigate(['registration'], {
+      queryParams: { id: eventId, name: eventName }
     });
   }
 
-
-  onOpenEvent(eventId: number, eventName: string) {
-    this.router.navigate(['registration'], { queryParams: { id: eventId, name: eventName } });
-  }
-
   onOpenRegistratedUsers(eventId: number, eventName: string) {
-    this.router.navigate(['registrated-users'], { queryParams: { id: eventId, name: eventName } });
+    this.router.navigate(['registrated-users'], {
+      queryParams: { id: eventId, name: eventName }
+    });
   }
-
 }
--- receptionistjs-ui/src/app/registrated-users/registrated-users.page.ts
+++ receptionistjs-ui/src/app/registrated-users/registrated-users.page.ts
@@ -1,34 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-registrated-users',
-  templateUrl: './registrated-users.page.html',
-  styleUrls: ['./registrated-users.page.scss'],
+  selector: "app-registrated-users",
+  templateUrl: "./registrated-users.page.html",
+  styleUrls: ["./registrated-users.page.scss"]
 })
 export class RegistratedUsersPage implements OnInit {
-
   eventId: number;
   eventName: string;
   eventAttendees: any = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private loadingController: LoadingController
+  ) {}
 
-  constructor(private route: ActivatedRoute,
-              private http: HttpClient) { }
-
-  ngOnInit() {
+  async ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.eventId = params['id'];
-      this.eventName = params['name'];
-  });
-  console.log(this.eventId);
+      this.eventId = params["id"];
+      this.eventName = params["name"];
+    });
+    console.log(this.eventId);
 
-  this.http.get(`http://localhost:5000/events/${this.eventId}`).toPromise().then((value) => {
-    console.log(value);
-    this.eventAttendees = value;
-  }).catch((err) => {
-    console.log(err);
-  });
+    const loading = await this.loadingController.create({
+      message: "Please wait..."
+    });
+    await loading.present();
+    this.http
+      .get(`http://localhost:5000/events/${this.eventId}`)
+      .toPromise()
+      .then(value => {
+        console.log(value);
+        this.eventAttendees = value;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
   }
 }
